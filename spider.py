280ac4372facf9ffd7760e55d6f27f28d36c9dd1
ACH4Q <achaqmohammedamine1@gmail.com>
2025-10-14 12:14:25 +0100

    Parsing

--- spider.py
+++ spider.py
@@ -3,9 +3,17 @@ import os
 import requests
 
 def main():
+        
+        images = ['.jpg','.jpeg','.gif','.bmp','.png']
         parser = argparse.ArgumentParser(
                 prog="spider",
                 description="spider is a web scarper programme",
                 epilog="thank you for using my programme"
         )
-        parser.add_argument('-r' help="add recursive scan of the target")
+        parser.add_argument('-r', help="add recursive scan of the target")
+        parser.add_argument('-l', help="indicates the maximum depth level of the recursive download")
+        parser.add_argument('-p',default='./data/' , help="insert the path where the downloaded files will be stored")
+
+        request = requests.request(
+                data=""
+        )
